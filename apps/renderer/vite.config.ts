b5f9d79bf977db7a7d04fac42df2d4d6602a1f61
ryuichi24 <ryuichi.nishi24@gmail.com>
2023-06-25 14:04:02 +0900

    feat: add env local

--- apps/renderer/vite.config.ts
+++ apps/renderer/vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig((env) => ({
-  base: env.mode === "production" ? "./" : "/",
-  build: {
-    sourcemap: "inline",
-    outDir: path.resolve(__dirname, "dist"),
-  },
-  root: "src",
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-}));
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.mode === "production" ? "./" : "/",
+    build: {
+      sourcemap: "inline",
+      outDir: path.resolve(__dirname, "dist"),
+    },
+    root: "src",
+    plugins: [react()],
+    server: {
+      port: env.VITE_DEV_SERVER_PORT || 3000,
+    },
+  } as UserConfig;
+});
