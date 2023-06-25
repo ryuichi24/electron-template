e02e3d9795b9021224b1ee179607d4f39672e32e
ryuichi24 <ryuichi.nishi24@gmail.com>
2023-06-25 14:51:02 +0900

    format

--- apps/renderer/src/main.tsx
+++ apps/renderer/src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import "@project/tailwind-config/global.css"
+import "@project/tailwind-config/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <App />,
 );
