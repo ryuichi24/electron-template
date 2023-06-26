cb9a77310b1e612c68616a98fcc8ace9a66cef38
ryuichi24 <ryuichi.nishi24@gmail.com>
2023-06-27 02:08:08 +0900

    fix: build fail

--- apps/renderer/vite.config.ts
+++ apps/renderer/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
   return {
-    base: env.mode === "production" ? "./" : "/",
+    base: mode === "production" ? "./" : "/",
     build: {
       sourcemap: "inline",
       outDir: path.resolve(__dirname, "dist"),
